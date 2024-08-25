c107512142822ec50788b53c54e24a746a84826e
iczky <fiqra22@gmail.com>
2024-08-25 12:53:30 +0700

    feat: Create constant for the url endpoint

---
+++ constants/url.ts
@@ -0,0 +1,12 @@
+export const config = {
+    BASE_URL: process.env.BASE_URL || 'http://localhost:8080/api/v1',
+    endpoints: {
+        auth: {
+            login: "/auth/login",
+            register: "/auth/register",
+            logout: "/auth/logout",
+            checkEmail: "/auth/check-email",
+            generateToken: "/auth/generate-token",
+        }
+    }
+}
