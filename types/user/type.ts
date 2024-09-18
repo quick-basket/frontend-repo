244a9c5fa60c02d62620f800f719298f9986af43
Fiqra Wardana <fiqra22@gmail.com>
2024-09-18 15:03:10 +0700

    update user address (#26)
    * feat: create location context to save current store id and user addresses
    
    * feat: create a location selection dialog for choosing the address for the first time
    
    * feat: integrate the context to the navbar and homepage component
    
    * feat: fix small params and method
    
    * feat: add hook logout for clearing all fetch data and blacklist the sid jwt
    
    * feat: add logout api for blacklist jwt sid. also check fot protected route to not give the sid
    
    * feat: clear all the location data for new user
    
    * feat: add dialog handler for showing the user address modal
    
    * feat: add the location context MORE context
    
    * feat: add all of that inside the component
    
    * feat: add some warning and animation to set address
    
    * feat: adjust navbar desktop so user can change their address
    
    * feat: add component to user add address
    
    * feat: add notiflixConfig.ts for updated notify
    
    * feat: implement service user address in front end and hook
    
    * feat: deleted now not closing the dialog
    
    ---------
    
    Co-authored-by: DioAlfino <86925167+DioAlfino@users.noreply.github.com>

--- types/user/type.ts
+++ types/user/type.ts
@@ -9,11 +9,12 @@ export interface ProfileType {
 export interface UserAddressType {
     id?: number;
     userId?: number;
+    name?: string;
     address: string;
     city: string;
     province: string;
     postalCode: string;
-    latitude?: number;
-    longitude?: number;
+    latitude: number;
+    longitude: number;
     isPrimary: boolean;
 }
