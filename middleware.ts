838c4fb1dc8e2f4f85613dcda1a4106ade519ca2
iczky <fiqra22@gmail.com>
2024-10-12 14:47:15 +0700

    TRY DEPLOY TO VERCEL #13

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 // export { auth as middleware } from "@/auth"
-import { NextResponse} from "next/server";
+import {NextResponse} from "next/server";
 import { auth } from "@/auth"; // Make sure this path is correct
 
 export const config = {
-    matcher: ['/((?!api/auth|_next/static|_next/image|favicon.ico).*)'],
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
 
 export default auth((req) => {
