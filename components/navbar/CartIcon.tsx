0a9d4266a630a76b878b5480f6ee77bc1c9c7ac9
iczky <fiqra22@gmail.com>
2024-10-13 19:42:12 +0700

    feat: adjust cart icon for mobile and not

--- components/navbar/CartIcon.tsx
+++ components/navbar/CartIcon.tsx
@@ -2,14 +2,24 @@ import React from 'react';
 import {ShoppingBasket} from "lucide-react";
 import useCart from "@/hooks/cart/useCart";
 
-const CartIcon = () => {
-    const { data: cartItems } = useCart();
+interface IconProps {
+    isMobile: boolean
+}
+
+const CartIcon: React.FC<IconProps> = ({isMobile}) => {
+    const {data: cartItems} = useCart();
     console.log("cart items", cartItems)
     const itemCount = cartItems?.length || 0;
 
     return (
         <div className="relative">
-            <ShoppingBasket className="text-gray-600" size={24}/>
+            {
+                isMobile ? (
+                    <ShoppingBasket className="text-white" size={24}/>
+                ) : (
+                    <ShoppingBasket className="text-gray-600" size={24}/>
+                )
+            }
             {itemCount > 0 && (
                 <div
                     className="absolute -top-2 -right-2 bg-red-600 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
